19b7f21f4016ac191bf9ef8b388ca6f359397964
Devisetty Dakkan Sai Nath <devisettydakkansainath@Devisettys-MacBook-Air.local>
2025-04-20 12:55:07 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ const express = require('express')
 const app = express(); ///creating new web server 
 
 
-//if we not specify the path then it always gives the result
+//if we not specify the path then it always gives the same result
 app.use("/home", (req,res) => {
-    res.send("Hello from the server")
+    res.send("Hello from the server...")
 })
 
-app.use("/", (req,res)=> {
+app.use("/hello", (req,res)=> {
     res.send("hello from the dashboard")
+})
+
+app.get("/abc*d", (req,res) => {
+    res.send({f_n: "sainath", l_n: "devisetty"})
+})
+
+app.post("/user", (req,res) => {
+    //send the data to DB
+    res.send( "Data successfully saved to database!!!");
+})
+
+app.delete("/user", (req, res) => {
+    //delete data from the db
+    res.send("Deleted successfully");
 })
 
 
